034bcf9efe8379e64ac881d006019fbf5c5306ee
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-24 15:21:58 -0400

    Created the CollectionPreview component

--- src/Pages/ShopPage/ShopPage.js
+++ src/Pages/ShopPage/ShopPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import shopData from './shopData';
 
+import CollectionPreview from '../../Components/CollectionPreview/CollectionPreview';
+
 class ShopPage extends React.Component {
   constructor() {
     super();
@@ -12,7 +14,15 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return <div>SHOP PAGE</div>;
+    const { collections } = this.state;
+
+    return (
+      <div>
+        {collections.map(({ id, ...collectionProps }) => (
+          <CollectionPreview key={id} {...collectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
