c08fb925ff0b2a2743d3c51e733928ac9860ad3b
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-24 13:45:45 -0400

    Created the DirectoryItem component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 
-import HomePage from './HomePage';
+import HomePage from './Pages/HomePage/HomePage';
 
 const App = () => (
   <div>
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './HomePage.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory'></div>
+  </div>
+);
+
+export default HomePage;
