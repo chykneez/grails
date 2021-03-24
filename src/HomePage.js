633e5c78c1c31bf7cda1f538609af1d871a753f3
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-24 13:24:18 -0400

    Created the HomePage component

---
+++ src/HomePage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory'>
+      <div className='directory-item'>
+        <div className='content'>
+          <h1 className='title'>Air Jordan</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='directory-item'>
+        <div className='content'>
+          <h1 className='title'>Nike</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='directory-item'>
+        <div className='content'>
+          <h1 className='title'>Adidas</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='directory-item'>
+        <div className='content'>
+          <h1 className='title'>Off-White</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='directory-item'>
+        <div className='content'>
+          <h1 className='title'>Yeezy</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
