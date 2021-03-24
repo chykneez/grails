c7e53b611bf9a9674a21bf4056c13575fa5602b6
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-24 15:37:46 -0400

    Added styling to the CollectionItem component

--- src/Components/CollectionPreview/CollectionPreview.js
+++ src/Components/CollectionPreview/CollectionPreview.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import CollectionItem from '../CollectionItem/CollectionItem';
+
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {items
         .filter((item, index) => index < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .map((id, ...itemProps) => (
+          <CollectionItem key={id} {...itemProps} />
         ))}
     </div>
   </div>
