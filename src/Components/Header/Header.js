693d9cfeabb2f67abe94cf5b830f219ea14b5b80
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-24 16:00:40 -0400

    Added styling to the Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Header.scss';
+
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 
 const Header = () => (
   <div className='header'>
     <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+      <Logo />
     </Link>
 
     <div className='options'>
