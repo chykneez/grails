090571a2ee1be1869276f6c93b4afdeb176496bb
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-25 10:57:33 -0400

    Updated the CartIcon component to display the count of the number of items in the cart

--- src/Components/CartIcon/CartIcon.js
+++ src/Components/CartIcon/CartIcon.js
@@ -7,15 +7,19 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-icon.svg';
 
 import { toggleCartDropdown } from '../../redux/cart/cartActions';
 
-const CartIcon = ({ toggleCartDropdown }) => (
+const CartIcon = ({ toggleCartDropdown, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartDropdown}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdown())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
