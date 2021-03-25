90147fce772185b435767b2dce19e622b359724b
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-25 09:55:33 -0400

    Created the CartDropdown component

---
+++ src/Components/CartDropdown/CartDropdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import CustomButton from '../CustomButton/CustomButton';
+
+const CartDropdown = () => (
+  <div className='cart-dropdown'>
+    <div className='cart-items' />
+    <CustomButton>CHECKOUT</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
