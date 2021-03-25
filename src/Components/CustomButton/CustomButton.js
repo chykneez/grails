bf7459dfef75f18fd9b4d370de9381b6a6a54875
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-25 10:18:50 -0400

    Updated styling on the CustomButton component for inverted buttons

--- src/Components/CustomButton/CustomButton.js
+++ src/Components/CustomButton/CustomButton.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 import './CustomButton.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? 'google' : ''} custom-button`}
+    className={`${isGoogleSignIn ? 'google' : ''} ${
+      inverted ? 'inverted' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
