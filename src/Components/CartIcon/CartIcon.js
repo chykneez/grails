7bb67e559250403eb99e6792b11b81fb50415515
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-26 19:59:31 -0400

    Converted the CartIcon component to implement styled components

--- src/Components/CartIcon/CartIcon.js
+++ src/Components/CartIcon/CartIcon.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './CartIcon.scss';
-
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-icon.svg';
+import {
+  CartContainer,
+  ShoppingIcon,
+  ItemCountContainer
+} from './CartIconStyles';
 
 import { toggleCartDropdown } from '../../redux/cart/cartActions';
 import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
 
 const CartIcon = ({ toggleCartDropdown, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartDropdown}>
-    <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>{itemCount}</span>
-  </div>
+  <CartContainer onClick={toggleCartDropdown}>
+    <ShoppingIcon />
+    <ItemCountContainer>{itemCount}</ItemCountContainer>
+  </CartContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
