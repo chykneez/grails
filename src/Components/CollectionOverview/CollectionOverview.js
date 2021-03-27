cf527b13fe764112220bb8e02d672803b75864e0
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-26 20:21:41 -0400

    Converted the CollectionOverview component to implement styled components

--- src/Components/CollectionOverview/CollectionOverview.js
+++ src/Components/CollectionOverview/CollectionOverview.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './CollectionOverview.scss';
+import { CollectionOverviewContainer } from './CollectionOverviewStyles';
 
 import CollectionPreview from '../CollectionPreview/CollectionPreview';
 
 import { selectShopCollectionsForPreview } from '../../redux/shop/shopSelectors';
 
 const CollectionOverview = ({ collections }) => (
-  <div className='collection-overview'>
+  <CollectionOverviewContainer>
     {collections.map(({ id, ...collectionProps }) => (
       <CollectionPreview key={id} {...collectionProps} />
     ))}
-  </div>
+  </CollectionOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
