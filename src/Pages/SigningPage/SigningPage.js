e243d6033e89e89add58c4dcf46ef15fc64df599
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-26 20:56:58 -0400

    Converted the SigningPage component to implement styled components

--- src/Pages/SigningPage/SigningPage.js
+++ src/Pages/SigningPage/SigningPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import './SigningPage.scss';
+import { SigningPageContainer } from './SigningPageStyles';
 
 import SignIn from '../../Components/SignIn/SignIn';
 import SignUp from '../../Components/SignUp/SignUp';
 
 const SigningPage = () => (
-  <div className='signing-page'>
+  <SigningPageContainer>
     <SignIn />
     <SignUp />
-  </div>
+  </SigningPageContainer>
 );
 
 export default SigningPage;
