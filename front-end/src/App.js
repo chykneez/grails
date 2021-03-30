41dce476a8218f6d5428481733263375792df486
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-03-30 10:35:07 -0400

    Implemented styled components to the App component and made it mobile friendly

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import { GlobalStyle } from './AppStyles';
 
 import Header from './Components/Header/Header';
 import HomePage from './Pages/HomePage/HomePage';
@@ -21,6 +21,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
